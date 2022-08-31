cada23f1cc8dd405741a763bb4f5ce38f666f80c
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-08-31 22:09:33 +0300

    число а возводим в степень в с помощью цикла

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число А");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число В");
+int b = int.Parse(Console.ReadLine());
+
+for(int i = 1;i <= b; i ++ )
+{
+   Console.WriteLine( Math.Pow(a,i));
+                                      }
